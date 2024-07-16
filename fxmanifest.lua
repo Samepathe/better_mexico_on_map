5a0d6b9e2f04542d2c998c6b44c3becea67103e1
Samaël <95944440+Samepathe@users.noreply.github.com>
2024-07-16 11:43:53 +0200

    Delete fxmanifest.lua

--- fxmanifest.lua
+++
@@ -1,7 +0,0 @@
-game 'rdr3'
-fx_version 'adamant'
-rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-
-author 'Samaël'
-description 'This is to show Mexico in your map in good quality'
-version '1.0.0'
